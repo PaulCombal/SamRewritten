185a6a5b7a604520773d1eb607da3ecf03186290
PaulCombal <abonnementspaul@gmail.com>
2025-07-30 19:25:48 +0200

    Update snapcraft

--- package/rebuild_snap.sh
+++ package/rebuild_snap.sh
@@ -2,10 +2,13 @@
 
 # This is a dev script
 # Launch this script from the project root directory
-# So far the Snap version is not working in strict mode. The dev mode is used for screenshots.
+# Classic confinement is for production.
+# Strict with dev mode is used for screenshots.
 
 rm *.snap
 snap remove samrewritten
 snapcraft
-snap install --devmode --dangerous *.snap
+#snap install --devmode --dangerous *.snap
 #snap install --dangerous *.snap
+#snap connect samrewritten:access-steam-folder
+snap install *.snap --classic --dangerous
